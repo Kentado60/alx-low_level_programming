eea302b1d659ecad5032e4153687a6bf126c3f7d
Kentado60 <kentado_60@DESKTOP-3638T18.localdomain>
2022-09-12 14:32:46 +0100

    Added task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (void)
+{
+	char string[70]="\"Programming is like building a multilingual puzzle\""
+	puts(string);
+	/* tried prnting it directly*/
+	puts("\"Programming is like building a multilingual puzzle\"")
+	return (0)
+}
