742291c8222c34270992216503e280885f268b41
Kentado60 <kentado_60@DESKTOP-3638T18.localdomain>
2022-09-12 14:51:08 +0100

    Added task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#!/bin/bash
-gcc 4-puts.c
 #include <stdio.h>
 int main (void)
 {
-	char string[70]="\"Programming is like building a multilingual puzzle\""
+	char string[70]="\"Programming is like building a multilingual puzzle\"";
 	puts(string);
 	/* tried prnting it directly*/
-	puts("\"Programming is like building a multilingual puzzle\"")
+	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0)
 }
 
