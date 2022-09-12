18451e8afa433d4c41fc50a7e872836e33541ecb
Kentado60 <kentado_60@DESKTOP-3638T18.localdomain>
2022-09-12 23:12:35 +0100

    Added task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+*/
+
 int main(void)
 {
-	char string[70] = "\"Programming is like building a multilingual puzzle\n";
-
-	puts(string);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
