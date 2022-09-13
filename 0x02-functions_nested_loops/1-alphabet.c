bcf6ba2cc5148ec29ee8600c28504855bb65b066
Kentado60 <kentado_60@DESKTOP-3638T18.localdomain>
2022-09-13 13:57:59 +0100

    Added task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - Entry point
+ * Description: a funtion that prints all alphabet in lower case
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+
+		putchar(i);
+	}
+	putchar('\n');
+}
