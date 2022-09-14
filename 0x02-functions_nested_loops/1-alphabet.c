446af56331a94a755b562f19ce0d65a008243df7
Kentado60 <kentado_60@DESKTOP-3638T18.localdomain>
2022-09-14 19:43:01 +0100

    Added task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_alphabet - Entry point
@@ -15,7 +14,6 @@ void print_alphabet(void)
 	{
 		putchar(i);
 	}
-	putchar(print_alphabet'\n');
+	putchar('\n');
 
-	return (void);
 }
