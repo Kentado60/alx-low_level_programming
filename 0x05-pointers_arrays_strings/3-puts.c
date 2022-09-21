63bcb9438935dace329b8a5c852e568dcd2f2d49
Kentado60 <kentado_60@DESKTOP-3638T18.localdomain>
2022-09-21 03:24:00 +0100

    Added to task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - prints out string to stdout
+ * @str: string to be printed out
+ *
+ * Return: Void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+
+	}
+	_putchar(10);
+
+}
+
+
